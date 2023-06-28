943008af88c96a8bc32e80331745a584de4d43a4
alves00 <matheusa.santos00@gmail.com>
2023-06-28 20:46:37 +0000

    primeira aula - criacao s3

---
+++ 01-primeira-aula/main.tf
@@ -0,0 +1,24 @@
+terraform {
+	required_version = "1.5.1"
+
+	required_providers {
+		aws {
+			source = "hashicorp/aws"
+			version = "5.5.0"
+		}
+	}
+}
+
+provider "aws" {
+	region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "my-tf-test-bucket-1254612312"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    Managedby = "Terraform"
+  }
+}
