4db930d3b33762f6227cf3adfee9fd567bba0487
alves00 <matheusa.santos00@gmail.com>
2023-06-29 00:55:56 +0000

    Change content/type - object

--- 01-terraform-basico/04-interpolacao-locals-outputs/s3.tf
+++ 01-terraform-basico/04-interpolacao-locals-outputs/s3.tf
@@ -8,6 +8,7 @@ resource "aws_s3_object" "this" {
   bucket = aws_s3_bucket.this.bucket
   key    = "config/${local.ip_filepath}"
 
-  source = local.ip_filepath
-  etag   = filemd5(local.ip_filepath)
+  source       = local.ip_filepath
+  etag         = filemd5(local.ip_filepath)
+  content_type = "application/json"
 }
